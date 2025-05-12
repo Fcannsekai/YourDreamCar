3f02107a0d7c787f04514571a8dabb6e1f736f70
Freddie <freddie.j.canning@gmail.com>
2025-05-12 14:37:03 -0700

    updates
    still need to fix code

--- YourDreamCar/src/main/java/com/dreamcar/data/DealershipFileManager.java
+++ YourDreamCar/src/main/java/com/dreamcar/data/DealershipFileManager.java
@@ -1,4 +1,13 @@
 package com.dreamcar.data;
 
+import com.dreamcar.model.*;
+
 public class DealershipFileManager {
+    public void getDealership() {
+        // Read from Dealership.csv and build Dealership object
+    }
+
+    public void saveDealership(Dealership dealership) {
+        // Write Dealership data to file
+    }
 }
--- YourDreamCar/src/main/java/com/dreamcar/model/Dealership.java
+++ YourDreamCar/src/main/java/com/dreamcar/model/Dealership.java
@@ -1,4 +1,22 @@
 package com.dreamcar.model;
 
+import com.dreamcar.ui.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private List<User.Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone, List<User.Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+    }
+
+    // Constructor and methods: addVehicle, removeVehicle, searchByPrice, etc.
 }
